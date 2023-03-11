f22f4a53861572673b762739b7b4d6c8f2828817
infdev <infdev@yandex.ru>
2023-03-11 23:43:23 +0400

    Логика из store и update контроллеров вынесена в сервис

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -2,27 +2,14 @@
 
 namespace App\Http\Controllers\Admin\Post;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\StoreRequest;
-use App\Models\Post;
-use Illuminate\Support\Facades\Storage;
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
     public function __invoke(StoreRequest $request)
     {
-        try {
-            $data = $request->validated();
-            $data['preview_image'] = Storage::disk('public')->put('images', $data['preview_image']);
-            $data['main_image'] = Storage::disk('public')->put('images', $data['main_image']);
-            $tagsIds = $data['tag_ids'];
-            unset($data['tag_ids']);
-
-            $post = Post::firstOrCreate($data);
-            $post->tags()->attach($tagsIds);
-        } catch (\Exception $exception) {
-            abort(404);
-        }
+        $data = $request->validated();
+        $this->service->store($data);
 
         return redirect()->route('admin.post.index');
     }
--- app/Http/Controllers/Admin/Post/UpdateController.php
+++ app/Http/Controllers/Admin/Post/UpdateController.php
@@ -2,23 +2,15 @@
 
 namespace App\Http\Controllers\Admin\Post;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Post\UpdateRequest;
 use App\Models\Post;
-use Illuminate\Support\Facades\Storage;
 
-class UpdateController extends Controller
+class UpdateController extends BaseController
 {
     public function __invoke(UpdateRequest $request, Post $post)
     {
         $data = $request->validated();
-        $data['preview_image'] = Storage::disk('public')->put('images', $data['preview_image']);
-        $data['main_image'] = Storage::disk('public')->put('images', $data['main_image']);
-        $tagsIds = $data['tag_ids'];
-        unset($data['tag_ids']);
-
-        $post->update($data);
-        $post->tags()->sync($tagsIds);
+        $post = $this->service->update($data, $post);
 
         return redirect()->route('admin.post.show', $post->id);
     }
